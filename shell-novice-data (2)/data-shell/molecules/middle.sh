ab9f437bb6e0426984b6d0f22d1752c821e4cdc6
Terry Uni <tmar080@aucklanduni.ac.nz>
2016-05-18 14:29:01 +1200

    make

--- shell-novice-data (2)/data-shell/molecules/middle.sh
+++ shell-novice-data (2)/data-shell/molecules/middle.sh
@@ -1,2 +1,6 @@
-head -n 15 octane.pdb | tail -n 5
+# Select lines from the middle of a file. 
+# Usage: middle.sh filename end_line num_lines
+#head -n "$2" "$1" | tail -n "$3"
+wc -l "$@" |sort -n
+
 
